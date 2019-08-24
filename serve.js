6905a2a97ff454a91c6c896ea6564ad9e3ddb4b1
MaxAkonde <devdevmaster@gmail.com>
2019-08-24 11:32:12 +0100

    Robot en module

--- serve.js
+++ serve.js
@@ -1,5 +1,4 @@
-var readline = require('readline');
-var rl = readline.createInterface(process.stdin, process.stdout);
+var questionHandler = require('./questionHandler');
 
 var questions = [
     "Comment t'appelles-tu ? ",
@@ -7,30 +6,10 @@ var questions = [
     "Quel est ton langage de programmation préféré ? "
 ];
 
-var person = {};
+
 var attributes = ['name', 'hobby', 'lg'];
-var actualIndex = 0;
 
-function getQuestion() {
-    return questions[actualIndex];
-}
-
-function ask() {
-    rl.question(getQuestion(), function(answer) {
-        person[attributes[actualIndex]] = answer;
-        actualIndex++;
-
-        if (actualIndex === questions.length) {
-            rl.close();
-        }
-
-        ask();
-    });
-}
-
-rl.on('close', function() {
-    console.log(person);
-    process.exit();
-});
-
-ask();
+questionHandler
+    .setQuestions(questions)
+    .setAttributes(attributes)
+    .ask();
